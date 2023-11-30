f338c34b12120e6423e89498a56712b9fccf039f
OmarElbanna <elbanna16@gmail.com>
2023-12-01 00:32:32 +0200

    Created Splash Screen

---
+++ lib/Splash.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 341,
+          width: 466,
+          child: Image.asset("images/Logo2.png"),
+        ),
+         SpinKitFadingCircle(
+          color: Colors.blueGrey[700],
+        ),
+      ],
+    ));
+  }
+}
