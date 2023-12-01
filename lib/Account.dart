f0832fb6e51170dbb10a7826df19e3740b5abcf9
OmarElbanna <elbanna16@gmail.com>
2023-12-02 01:34:32 +0200

    Finalized Update Account Functionality

--- lib/Account.dart
+++ lib/Account.dart
@@ -1,6 +1,8 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({super.key});
@@ -10,9 +12,28 @@ class AccountScreen extends StatefulWidget {
 }
 
 class _AccountScreenState extends State<AccountScreen> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController firstName = TextEditingController();
+  TextEditingController lastName = TextEditingController();
+  TextEditingController phone = TextEditingController();
   bool passwordVisible = true;
   late User user;
-  List<Map> data = [];
+
+  String? validateName(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'This field is required';
+    }
+    return null;
+  }
+
+  String? validatePhone(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Phone number is required';
+    } else if (!RegExp(r'^(010|011|012|015)\d{8}$').hasMatch(value)) {
+      return 'Enter a valid 11-digit phone number';
+    }
+    return null;
+  }
 
   @override
   void initState() {
@@ -34,36 +55,42 @@ class _AccountScreenState extends State<AccountScreen> {
         ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: FutureBuilder<DocumentSnapshot>(
-              future: FirebaseFirestore.instance
-                  .collection('users')
-                  .doc(user.uid)
-                  .get(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                }
-                if (!snapshot.hasData || !snapshot.data!.exists) {
-                  return const Text('User data not found');
-                }
-                final userData = snapshot.data!.data() as Map<String, dynamic>;
-                return Column(
+      body: FutureBuilder<DocumentSnapshot>(
+        future:
+            FirebaseFirestore.instance.collection('users').doc(user.uid).get(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+          if (!snapshot.hasData || !snapshot.data!.exists) {
+            return const Text('User data not found');
+          }
+          final userData = snapshot.data!.data() as Map<String, dynamic>;
+          firstName.text = userData['firstName'];
+          lastName.text = userData['lastName'];
+          phone.text = userData['phone'];
+
+          return Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
                   children: [
-                  SizedBox(
-                  height: 140,
-                  child: Image.asset("images/download.png"),
-                ),
+                    SizedBox(
+                      height: 140,
+                      child: Image.asset("images/download.png"),
+                    ),
                     const SizedBox(height: 20),
                     TextFormField(
-                      initialValue: userData['firstName'],
+                      validator: validateName,
+                      controller: firstName,
+                      // initialValue: userData['firstName'],
                       keyboardType: TextInputType.name,
                       decoration: const InputDecoration(
                         label: Text("First Name"),
@@ -75,8 +102,10 @@ class _AccountScreenState extends State<AccountScreen> {
                       height: 20,
                     ),
                     TextFormField(
+                      validator: validateName,
+                      controller: lastName,
                       keyboardType: TextInputType.name,
-                      initialValue: userData['lastName'],
+                      // initialValue: userData['lastName'],
                       decoration: const InputDecoration(
                         label: Text("Last Name"),
                         border: OutlineInputBorder(
@@ -87,8 +116,10 @@ class _AccountScreenState extends State<AccountScreen> {
                       height: 20,
                     ),
                     TextFormField(
+                      validator: validatePhone,
+                      controller: phone,
                       keyboardType: TextInputType.phone,
-                      initialValue: userData['phone'],
+                      // initialValue: userData['phone'],
                       decoration: const InputDecoration(
                         suffixIcon: Icon(Icons.phone),
                         label: Text("Phone Number"),
@@ -120,7 +151,35 @@ class _AccountScreenState extends State<AccountScreen> {
                     // ),
                     const SizedBox(height: 20),
                     MaterialButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        if (_formKey.currentState?.validate() ?? false) {
+                          FirebaseFirestore.instance
+                              .collection('users')
+                              .doc(user.uid)
+                              .update({
+                            'firstName': firstName.text,
+                            'lastName': lastName.text,
+                            'phone': phone.text
+                          }).then((value) => AwesomeDialog(
+                            context: context,
+                            dialogType: DialogType.success,
+                            animType: AnimType.rightSlide,
+                            title: 'Success',
+                            desc:
+                            'Successfully updated user account',
+                            btnOkOnPress: () {},
+                          )..show())
+                            .catchError((error) => AwesomeDialog(
+                            context: context,
+                            dialogType: DialogType.error,
+                            animType: AnimType.rightSlide,
+                            title: 'Error',
+                            desc:
+                            'Something wrong happened, please try again later',
+                            btnOkOnPress: () {},
+                          )..show());
+                        }
+                      },
                       color: Colors.blueGrey[700],
                       child: const Text(
                         "Save",
@@ -130,9 +189,11 @@ class _AccountScreenState extends State<AccountScreen> {
                       ),
                     ),
                   ],
-                );
-              },
-            )),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
