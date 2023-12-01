9d4510c669efcf292c9f70712991a71d81303462
OmarElbanna <elbanna16@gmail.com>
2023-12-01 23:38:18 +0200

    Retrieved user data in my account page

--- lib/Account.dart
+++ lib/Account.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({super.key});
@@ -9,11 +11,18 @@ class AccountScreen extends StatefulWidget {
 
 class _AccountScreenState extends State<AccountScreen> {
   bool passwordVisible = true;
+  late User user;
+  List<Map> data = [];
+
+  @override
+  void initState() {
+    super.initState();
+    user = FirebaseAuth.instance.currentUser!;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       resizeToAvoidBottomInset: true,
       appBar: AppBar(
         backgroundColor: Colors.blueGrey[700],
@@ -27,87 +36,103 @@ class _AccountScreenState extends State<AccountScreen> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              const CircleAvatar(
-                radius: 80,
-                backgroundImage: NetworkImage(
-                  "https://media.licdn.com/dms/image/C4D03AQGFxldRxlU7Xg/profile-displayphoto-shrink_800_800/0/1661131775382?e=2147483647&v=beta&t=A1qCwqFSQT44KYD1geoOwQlFP9uqBBNMUgN4NFtyfK8",
+            padding: const EdgeInsets.all(20),
+            child: FutureBuilder<DocumentSnapshot>(
+              future: FirebaseFirestore.instance
+                  .collection('users')
+                  .doc(user.uid)
+                  .get(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
+                if (!snapshot.hasData || !snapshot.data!.exists) {
+                  return const Text('User data not found');
+                }
+                final userData = snapshot.data!.data() as Map<String, dynamic>;
+                return Column(
+                  children: [
+                  SizedBox(
+                  height: 140,
+                  child: Image.asset("images/download.png"),
                 ),
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                initialValue: "Omar",
-                keyboardType: TextInputType.name,
-                decoration: const InputDecoration(
-                  label: Text("First Name"),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(5))),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.name,
-                initialValue: "Elbanna",
-                decoration: const InputDecoration(
-                  label: Text("Last Name"),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(5))),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.phone,
-                initialValue: "01011553108",
-                decoration: const InputDecoration(
-                  suffixIcon: Icon(Icons.phone),
-                  label: Text("Phone Number"),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(5))),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                initialValue: "123456789",
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: passwordVisible,
-                decoration: InputDecoration(
-                  label: const Text("Password"),
-                  suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          passwordVisible = !passwordVisible;
-                        });
-                      },
-                      icon: Icon(passwordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off)),
-                  border: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(5))),
-                ),
-              ),
-              const SizedBox(height: 20),
-              MaterialButton(
-                onPressed: () {},
-
-                color: Colors.blueGrey[700],
-                child: const Text(
-                  "Save",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      initialValue: userData['firstName'],
+                      keyboardType: TextInputType.name,
+                      decoration: const InputDecoration(
+                        label: Text("First Name"),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.name,
+                      initialValue: userData['lastName'],
+                      decoration: const InputDecoration(
+                        label: Text("Last Name"),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.phone,
+                      initialValue: userData['phone'],
+                      decoration: const InputDecoration(
+                        suffixIcon: Icon(Icons.phone),
+                        label: Text("Phone Number"),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    // TextFormField(
+                    //   initialValue: "123456789",
+                    //   keyboardType: TextInputType.visiblePassword,
+                    //   obscureText: passwordVisible,
+                    //   decoration: InputDecoration(
+                    //     label: const Text("Password"),
+                    //     suffixIcon: IconButton(
+                    //         onPressed: () {
+                    //           setState(() {
+                    //             passwordVisible = !passwordVisible;
+                    //           });
+                    //         },
+                    //         icon: Icon(passwordVisible
+                    //             ? Icons.visibility
+                    //             : Icons.visibility_off)),
+                    //     border: const OutlineInputBorder(
+                    //         borderRadius: BorderRadius.all(Radius.circular(5))),
+                    //   ),
+                    // ),
+                    const SizedBox(height: 20),
+                    MaterialButton(
+                      onPressed: () {},
+                      color: Colors.blueGrey[700],
+                      child: const Text(
+                        "Save",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            )),
       ),
     );
   }
