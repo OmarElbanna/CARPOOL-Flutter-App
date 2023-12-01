70316629ad9a408b2d12cd8ddc199460f913ffac
OmarElbanna <elbanna16@gmail.com>
2023-12-01 13:34:02 +0200

    Added Check current auth state

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'Account.dart';
@@ -8,21 +9,47 @@ import 'Signup.dart';
 import 'Splash.dart';
 import 'TripDetails.dart';
 
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      showSemanticsDebugger: false,
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => const SplashScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/signup_s': (context) => const SignupScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/account': (context) => const AccountScreen(),
+        '/mytrips': (context) => const TripsScreen(),
+        '/tripdetails': (context) => const TripDetailsScreen()
+      },
+    );
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MaterialApp(
-    showSemanticsDebugger: false,
-    debugShowCheckedModeBanner: false,
-    initialRoute: '/splash',
-    routes: {
-      '/splash': (context) => const SplashScreen(),
-      '/login': (context) => const LoginScreen(),
-      '/signup_s': (context) => const SignupScreen(),
-      '/home': (context) => const HomeScreen(),
-      '/account': (context) => const AccountScreen(),
-      '/mytrips': (context) => const TripsScreen(),
-      '/tripdetails': (context) => const TripDetailsScreen()
-    },
-  ));
+  runApp(const MyApp());
 }
